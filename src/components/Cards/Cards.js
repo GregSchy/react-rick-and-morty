dc54642c64547e4b9121896b10ab77343e991490
GregSchy <105386198+GregSchy@users.noreply.github.com>
2022-10-19 18:58:36 +0200

    3

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import styles from './Cards.module.scss'
+
+
 
 
 export const Cards = ({ results }) => {
@@ -7,11 +10,23 @@ export const Cards = ({ results }) => {
 
   if(results){
     display = results.map(x=>{
-        let{id, name} = x
-        return(  <div key={id} className="col-4">
-            {name}
-        </div>)
-    })
+        let{id, name, image, location, status} = x
+        return(  
+            <div key={id} className="col-4 position-relative">
+                <div className="">
+                    <img src={image} alt="" className="img-fluid" />
+                    <div className="content">
+                        <div className="fs-4 fw-bold mb-4">{name}</div>
+                        <div className="">
+                            <div className="fs-6">Last location</div>
+                            <div className="fs-5">{location.name}</div>
+                        </div>
+                    </div>
+                </div>
+                <div className={`${styles.badge} position-absolute badge bg-danger`}>{status}</div>   
+            </div>
+        );
+    });
   }
   else {
     display = "No Characters found ;/"
